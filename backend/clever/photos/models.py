38a8f5b5bbef38672e6fab0c8af89b3753bd2898
Felipe Faria <Felipe Faria>
2024-05-06 21:21:15 -0300

    feat: create photo models

---
+++ backend/clever/photos/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Photo(models.Model):
+    photographer = models.CharField(max_length=254)
+    photographer_url = models.URLField(max_length=2083, null=True, blank=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="photos")
+    image_url = models.URLField(max_length=2083, null=True, blank=True)
+    color = models.CharField(max_length=10, blank=True, null=True)
+    likes = models.ManyToManyField(User, through="Like", verbose_name="likes")
+
+    def __str__(self):
+        return self.description
+
+
+class Like(models.Model):
+    photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = [["photo", "user"]]
