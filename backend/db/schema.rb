57586f1375fc27fbb6695a4f3175eb3f22ea97ee
carolis <redrumjedi@gmail.com>
2025-06-30 21:26:32 -0300

    Remove unused table from the scaffold

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_29_165604) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_01_002428) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -37,13 +37,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_29_165604) do
     t.text "alt_text"
     t.index ["pexels_id"], name: "index_photos_on_pexels_id", unique: true
     t.index ["user_id"], name: "index_photos_on_user_id"
-  end
-
-  create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.boolean "completed"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
