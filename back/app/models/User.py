2cc1a29840e7bcce7276578913043c42aae5f9ac
Felipe Rocha De Lucena <felipe.lucena@quintoandar.com.br>
2025-07-03 20:27:24 -0300

    feat: create models and first migration

---
+++ back/app/models/User.py
@@ -0,0 +1,23 @@
+import sqlalchemy as sa
+import uuid
+
+from pydantic import EmailStr
+from sqlmodel import Field, SQLModel, Relationship
+from typing import List
+
+
+class UserAccount(SQLModel):
+    email: EmailStr = Field(max_length=255)
+    password: str = Field(min_length=8, max_length=40)
+
+
+class User(SQLModel, table=True):
+    id: uuid.UUID = Field(
+        primary_key=True,
+        nullable=False,
+        sa_column_kwargs={"server_default": sa.text("gen_random_uuid()")},
+    )
+    email: EmailStr = Field(unique=True, index=True, max_length=255)
+    hashed_password: str
+
+    liked_photos: List["PhotoLike"] = Relationship(back_populates="user")  # type: ignore  # noqa: F821
