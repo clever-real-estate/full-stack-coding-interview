e35278825698d27bc27b8ea5637f2d2eafe7535e
Felipe Rocha De Lucena <felipe.lucena@quintoandar.com.br>
2025-07-03 20:59:47 -0300

    feat: first route to list all photos

--- back/README.md
+++ back/README.md
@@ -3,3 +3,17 @@
 ```sh
 uv run fastapi dev
 ```
+
+### Migration
+
+```sh
+uv run alembic revision --autogenerate -m "<message>"
+
+uv run alembic upgrade head
+```
+
+### Importing photos
+
+```sh
+uv run python -m app.utils.import_photos
+```
--- back/app/main.py
+++ back/app/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from sqlmodel import Session, select
+from app.models import Photo
+from app.db import engine
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
+@app.get("/health-check")
+def health_check():
     return {"message": "Welcome to the Clever Photos API!"}
+
+
+@app.get("/photos/all", response_model=list[Photo])
+def get_all_photos():
+    with Session(engine) as session:
+        photos_statement = select(Photo)
+        photos = session.exec(photos_statement).all()
+        return photos
