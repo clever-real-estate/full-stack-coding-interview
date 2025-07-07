dffcd68d865e2bc2cae56c96479cc3c8f3ba7c25
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2025-07-07 19:10:43 -0400

    chore(backend): add core third-party and local apps to INSTALLED_APPS
    Added the following apps:
    - rest_framework: Django REST Framework for API creation
    - corsheaders: Handle cross-origin requests (CORS)
    - rest_framework_simplejwt: JWT authentication
    - django_filters: Filtering support for DRF
    - photo_gallery: Local app for photo data

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "corsheaders",
+    "rest_framework_simplejwt",
+    "django_filters",
+    "photo_gallery"
 ]
 
 MIDDLEWARE = [
