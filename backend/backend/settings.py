e0c9d34c0a0f2e64b5929e9f5e68ca5633ff5767
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2025-07-08 19:51:38 -0400

    chore(backend): add custom pagination class

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -178,8 +178,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": config("DEFAULT_PAGE_SIZE", default=10, cast=int),
+    "DEFAULT_PAGINATION_CLASS": "backend.pagination.CustomPageNumberPagination",
 }
 
 
