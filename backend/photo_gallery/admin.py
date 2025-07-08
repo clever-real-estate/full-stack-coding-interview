970e8f85b9ee557e1a33253904a4ffe299bd1034
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2025-07-07 20:32:18 -0400

    chore(models): register models in admin dashboard via `admin.py` file

--- backend/photo_gallery/admin.py
+++ backend/photo_gallery/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.conf import settings
+from .models import Photographer, Photo, Like
 
+
+admin.site.site_header = f"{settings.SITE_NAME} Admin"
+admin.site.register([Photographer, Photo, Like])
 # Register your models here.
