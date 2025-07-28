e55bdbfeb3e4aadeb88d747284d0a5f675b24cac
Victor Park <victor.shpark@protonmail.com>
2025-07-28 01:50:46 -0300

    feat(frontend): Add and configure Tailwind CSS and Vitest

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import { defineConfig } from 'vite';  
+import react from '@vitejs/plugin-react';  
+//import tailwind from 'tailwindcss';
+//import autoprefixer from 'autoprefixer';
+  
+export default defineConfig({  
+  plugins: [react()],  
+  test: {  
+    globals: true,  
+    environment: 'jsdom',  
+    setupFiles: './src/test/setup.ts',  
+  },  
+});
